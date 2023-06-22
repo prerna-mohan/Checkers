9aff2a3982aa6a7575c22499eaf369c29f5bea89
prerna-mohan <prerna271998@gmail.com>
2023-06-22 23:04:54 +0530

    Simple piece select and move

--- Assets/CheckersBoard.cs
+++ Assets/CheckersBoard.cs
@@ -9,9 +9,65 @@ public class CheckersBoard : MonoBehaviour
     public GameObject blackPiecePrefab;
     private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
     private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
+    private Vector2 mouseOver;
+    private Piece selectedPiece;
+    private Vector2 startDrag;
+    private Vector2 endDrag;
     private void Start()
     {
         GenerateBoard();
+    }
+
+    private void Update()
+    {
+        UpdateMouseOver();
+        //if its my turn
+        int x = (int)mouseOver.x;
+        int y = (int)mouseOver.y;
+        if (Input.GetMouseButtonDown(0))
+            SelectPiece(x, y);
+        if(Input.GetMouseButtonUp(0))
+            TryMove((int)startDrag.x, (int)startDrag.y,x,y);    
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2){
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        MovePiece(selectedPiece, x2, y2);
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        if (x < 0 || x >= pieces.Length || y < 0 || y >= pieces.Length)
+            return;
+        Piece p = pieces[x, y];
+        if (p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+        }
+    }
+
+    private void UpdateMouseOver()
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
     }
 
     private void GenerateBoard()
