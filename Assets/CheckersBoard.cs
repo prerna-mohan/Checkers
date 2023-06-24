70ee093b67a4aaaa794dbf4d70dbd4f02c8b99f3
prerna-mohan <prerna271998@gmail.com>
2023-06-24 23:59:40 +0530

    Trymove

--- Assets/CheckersBoard.cs
+++ Assets/CheckersBoard.cs
@@ -24,18 +24,54 @@ public class CheckersBoard : MonoBehaviour
         //if its my turn
         int x = (int)mouseOver.x;
         int y = (int)mouseOver.y;
+        if (selectedPiece != null)
+            UpdatePieceDrag(selectedPiece);
         if (Input.GetMouseButtonDown(0))
             SelectPiece(x, y);
-        if(Input.GetMouseButtonUp(0))
-            TryMove((int)startDrag.x, (int)startDrag.y,x,y);    
+        if (Input.GetMouseButtonUp(0))
+            TryMove((int)startDrag.x, (int)startDrag.y, x, y);
     }
 
-    private void TryMove(int x1, int y1, int x2, int y2){
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
         startDrag = new Vector2(x1, y1);
         endDrag = new Vector2(x2, y2);
         selectedPiece = pieces[x1, y1];
 
+        if (x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length)
+        {
+            if (selectedPiece != null)
+                MovePiece(selectedPiece, x1, y1);
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+        if (selectedPiece != null)
+        {
+            if (endDrag == startDrag)
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+        }
+        //check if valid move
         MovePiece(selectedPiece, x2, y2);
+    }
+
+    private void UpdatePieceDrag(Piece p)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
     }
 
     private void SelectPiece(int x, int y)
--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    public bool isWhite;
+    public bool isKing;
+    public bool validMove(Piece[,] board, int x1, int y1, int x2, int y2){
+        if(board[x2, y2] != null)
+            return false;
+
+        int deltaMove = Mathf.Abs(x1 - x2);
+        int deltaMoveY = y1 - y2;
+        if(isWhite || isKing){
+            if(deltaMove == 1){
+                if(deltaMoveY == 1)
+                    return true;
+            }
+            else if(deltaMove == 2){
+                if(deltaMoveY == 2){
+                    Piece p = board[(x1 + x2)/2, (y1 + y2)/2];
+                    if(p != null && p.isWhite != isWhite)
+                        return true;
+                }
+            }
+        } 
+        return false;   
+    }
     // Start is called before the first frame update
     void Start()
     {
